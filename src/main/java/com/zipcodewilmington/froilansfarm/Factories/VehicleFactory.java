5353488a1ffb5dd0a380b0b07a8e32dd9afcc602
Kyle Carter <elyko.tech@gmail.com>
2019-03-08 09:49:25 -0500

    added more uml changes

--- src/main/java/com/zipcodewilmington/froilansfarm/Factories/VehicleFactory.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Factories/VehicleFactory.java
@@ -1,4 +1,11 @@
 package com.zipcodewilmington.froilansfarm.Factories;
 
+import com.zipcodewilmington.froilansfarm.Interfaces.Vehicle;
+
+import java.util.List;
+
 public class VehicleFactory {
+    public List<Vehicle> createVehicle(Vehicle vehicleType, Integer numberofVehicles) {
+        return null;
+    }
 }
