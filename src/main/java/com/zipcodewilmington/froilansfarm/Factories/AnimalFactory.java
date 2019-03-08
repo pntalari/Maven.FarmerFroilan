943525e2b112f9c711eb06fa85ca27ff04041048
Kyle Carter <elyko.tech@gmail.com>
2019-03-08 11:26:47 -0500

    added more uml changes

--- src/main/java/com/zipcodewilmington/froilansfarm/Factories/AnimalFactory.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Factories/AnimalFactory.java
@@ -1,4 +1,13 @@
 package com.zipcodewilmington.froilansfarm.Factories;
 
+import com.zipcodewilmington.froilansfarm.Animals.Chicken;
+import com.zipcodewilmington.froilansfarm.Animals.Horse;
+
+import java.util.List;
+
 public class AnimalFactory {
+    public List<Chicken> createChicken(Integer numberOfChicken){
+        return null;
+    }
+    public List<Horse> createHorse(Integer numberOfHorse){return null;}
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Factories/VehicleFactory.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Factories/VehicleFactory.java
@@ -1,11 +1,15 @@
 package com.zipcodewilmington.froilansfarm.Factories;
 
-import com.zipcodewilmington.froilansfarm.Interfaces.Vehicle;
+import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
 
 import java.util.List;
 
 public class VehicleFactory {
-    public List<Vehicle> createVehicle(Vehicle vehicleType, Integer numberofVehicles) {
+    public List<Tractor> createTractor(Integer numberOfTractors) {
+        return null;
+    }
+    public List<CropDuster> createCropDuster(Integer numberOfTractors) {
         return null;
     }
 }
