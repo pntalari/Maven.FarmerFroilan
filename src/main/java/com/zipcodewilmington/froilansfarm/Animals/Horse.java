96fca7602fa4d537f3fc9e58ea31d56c950c90f2
Nirmala <nirmala.talari@gmail.com>
2019-03-08 13:14:28 -0500

    AnimalTests

--- src/main/java/com/zipcodewilmington/froilansfarm/Animals/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animals/Horse.java
@@ -1,21 +1,45 @@
 package com.zipcodewilmington.froilansfarm.Animals;
-
 import com.zipcodewilmington.froilansfarm.Interfaces.Animal;
 import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
 import com.zipcodewilmington.froilansfarm.Interfaces.Ridable;
+import com.zipcodewilmington.froilansfarm.Produce.EarCorn;
 
 public class Horse implements Animal, Ridable {
+    private Boolean isHorseRidden = false;
+    private Boolean isHorseFed = false;
+    private EarCorn horsefood;
+
+    public Horse() {
+        this.isHorseRidden = false;
+        this.horsefood = null;
+        this.isHorseFed = false;
+    }
+
+    public Horse(EarCorn corn) {
+        this.horsefood = corn;
+    }
 
     public String makeNoise() {
-
         return "neigh!";
     }
 
-    public void ride() {
-
-    }
-
     public void eat(Edible food) {
-
+        this.horsefood = (EarCorn) food;
+        if (horsefood != null && horsefood instanceof EarCorn) {
+            this.isHorseFed = true;
+        }
     }
+
+    public void ride() {
+        this.isHorseRidden = true;
+    }
+
+    public Boolean getHorseRidden() {
+        return this.isHorseRidden;
+    }
+
+    public Boolean getHorseFed() {
+        return this.isHorseFed;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/Animals/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animals/HorseTest.java
@@ -1,24 +1,47 @@
 package com.zipcodewilmington.froilansfarm.Animals;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+import com.zipcodewilmington.froilansfarm.Interfaces.Person;
+import com.zipcodewilmington.froilansfarm.People.Rider;
+import com.zipcodewilmington.froilansfarm.Produce.EarCorn;
 import org.junit.Assert;
 import org.junit.Test;
 
-class HorseTest {
+import static org.junit.Assert.*;
+
+public class HorseTest {
     Horse horseObj = new Horse();
+  //  Person personObj = new Rider();
 
     @Test
-    void makeNoiseTest() {
+   public void makeNoiseTest() {
+        //Given
         String expectedNoise = "neigh!";
+        //When
         String actualNoise = horseObj.makeNoise();
+        //Then
         Assert.assertEquals(expectedNoise, actualNoise);
     }
 
     @Test
-    void rideTest() {
-
+    public void eatTest() {
+        //Given
+        Edible food = new EarCorn();
+        Boolean beforeIsFed = false;
+        //When
+        horseObj.eat(food);
+        Boolean afterIsFed = horseObj.getHorseFed();
+        Assert.assertNotEquals(beforeIsFed,afterIsFed);
     }
 
     @Test
-    void eatTest() {
+    public void rideTest() {
+        //Given
+
+        //When
+        //Then
     }
+
+
 }
